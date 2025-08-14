78165b6fb15de9271421e1f58b50e29c6b4c4e69
Syed Md Ramiz <76583293+syedmdramiz@users.noreply.github.com>
2025-08-14 06:17:14 +0000

    conversion

---
+++ 01/3_conversion.js
@@ -0,0 +1,29 @@
+let score = "22"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+//console.log(typeof (valueInNumber));
+//console.log(valueInNumber);
+
+/*
+"22" => 33
+"33abv" => NaN
+true => 1; false => 0
+*/ 
+
+let isloggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isloggedIn)
+//console.log(booleanIsLoggedIn);
+
+//1 => true; false
+//"" => false
+//"hitesh" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
