7a68e37095aa4ece8de7e0096ef45ab6cbb13681
Syed Md Ramiz <76583293+syedmdramiz@users.noreply.github.com>
2025-08-14 05:34:36 +0000

    container config done

---
+++ 01/2_datatypes.js
@@ -0,0 +1,26 @@
+"use sctrict"; // treat all JS code as newer version
+
+// alert("Hello") // we are using Nodejs but browser
+
+console.log(3+3); 
+ 
+
+let name = "Ramiz"
+let age  = 22
+let isLoggedIn = false
+let state;
+
+
+/* number => 2 to the power 53
+bigint = 
+string => ""
+boolean => trues/false
+null=> standalone value
+undefined
+symbol=>unique
+
+object
+*/
+
+console.log(typeof undefined) // undefined
+console.log(typeof null)
