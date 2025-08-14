e460d46da611e5bfc342364e621f21de229651fb
Syed Md Ramiz <76583293+syedmdramiz@users.noreply.github.com>
2025-08-14 04:51:48 +0000

    conatainer config done

---
+++ 01/1_variables.js
@@ -0,0 +1,25 @@
+const accountId = 144533
+let accountEmail = "syedmdramiz@gmail.com"
+var accountPassword = "12345"
+accountCity = "Kolkata"
+let accountState;
+
+
+//accountId = 2 // not allowed
+
+accountEmail = "123@gmail.com"
+accountPassword = "456456"
+accountCity = "America"
+
+console.log(accountId);
+
+
+/*
+Use let to declare variables but var
+
+*/
+
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
